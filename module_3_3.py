8b7d56d2891801797b533c88f118e46bf14a2f67
SergeyL88 <74002827+SergeyL88@users.noreply.github.com>
2024-09-06 22:01:08 +0400

    Доработка "Распаковка позиционных параметров"

--- module_3_3.py
+++ module_3_3.py
@@ -5,6 +5,7 @@ value_list: list = [False, [15, 'String', True], 'Word']
 values_dict: dict = {'a': 0.8, 'b': False, 'c': 'Word'}
 value_list_2: list = [[1, 15, 36], 'Слово']
 
+print_params()
 print_params(b = 25)
 print_params(c = [1, 2, 3])
 print_params(*value_list)
